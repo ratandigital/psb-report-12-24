b8a9dc8e2fb91d1d3bc7ccdd396836e871eacc6b
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2024-12-13 09:15:26 +0600

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
+import { DropdownMenuBar } from "@/components/DropdownMenuBar";
+
 
 const DashboardLayout = async ({
   children,
@@ -10,6 +12,13 @@ const DashboardLayout = async ({
 
   return ( 
     <div className="h-full relative">
+      <div className="flex w-full justify-end">
+        <div className="mr-5"><DropdownMenuBar/></div>
+        <div><UserMenu/>
+
+        </div>
+ 
+      </div>
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
         <Sidebar />
       </div>
