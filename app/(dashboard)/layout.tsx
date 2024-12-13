e977d313d7a487c488020330dfca2e81c1e0d5d9
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2024-12-13 09:21:09 +0600

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,13 +12,7 @@ const DashboardLayout = async ({
 
   return ( 
     <div className="h-full relative">
-      <div className="flex w-full justify-end">
-        <div className="mr-5"><DropdownMenuBar/></div>
-        <div><UserMenu/>
-
-        </div>
- 
-      </div>
+     
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
         <Sidebar />
       </div>
