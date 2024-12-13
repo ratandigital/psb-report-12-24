300be41a1ec1745e9dd02598e9a8bc59133e32ec
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2024-12-13 11:29:20 +0600

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,7 +9,13 @@ const Navbar = async () => {
   return ( 
     <div className="flex items-center p-4">
       <MobileSidebar/>
-     
+      <div className="flex w-full justify-end">
+        <div className="mr-5"><DropdownMenuBar/></div>
+        <div><UserMenu/>
+
+        </div>
+ 
+      </div>
     
     </div>
        
